1f471a146c9c9125a14208050f22727d43d57883
madhvpruthi <pruthimadhav881@gmail.com>
2026-02-15 23:47:58 +0530

    Time: 0 ms (100%), Space: 22.2 MB (9.22%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        vector<int> ans;
+        inorder(root, ans);
+
+        for (int i = 1; i < ans.size(); i++) {
+            if (ans[i] <= ans[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private:
+    void inorder(TreeNode* node, vector<int> &ans){
+        if(node == NULL) return;
+        
+        inorder(node->left, ans );
+        ans.push_back(node->val);
+        inorder(node->right, ans);
+    }
+};
