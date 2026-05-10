e8c0a6f143abe346b16d42e3630cfd4e9871e3fc
Madhav Pruthi <pruthimadhav881@gmail.com>
2026-05-10 17:54:24 +0530

    Time: 2 ms (11.17%), Space: 9 MB (63.59%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int l =0, r=0; 
+        int blank=0;
+        for(char c:moves){
+            if(c=='L') l++;
+            else if(c=='R') r++;
+            else blank++;
+        }
+        return abs(l-r) + blank;
+    }
+};
