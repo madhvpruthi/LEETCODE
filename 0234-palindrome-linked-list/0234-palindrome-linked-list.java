83ed71bf17e2722db196008f562d05f34df8131e
Madhav Pruthi <pruthimadhav881@gmail.com>
2026-07-14 12:15:32 +0530

    Time: 3 ms (99.83%), Space: 94.6 MB (44.78%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,22 +10,56 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack <Integer> st = new Stack<>();
+//         Stack <Integer> st = new Stack<>();
 
-        ListNode temp = head;
-        while(temp!= null){
-            st.push(temp.val);
-            temp=temp.next;
-        }
+//         ListNode temp = head;
+//         while(temp!= null){
+//             st.push(temp.val);
+//             temp=temp.next;
+//         }
 
-        temp=head;
-        while (temp != null) {
-            if (temp.val != st.peek()) {
+//         temp=head;
+//         while (temp != null) {
+//             if (temp.val != st.peek()) {
+//                 return false;
+//             }
+//             st.pop();
+//             temp=temp.next;
+//         }
+//         return true;
+//     }
+// }
+    
+    ListNode slow = head;
+    ListNode fast = head;
+    while(fast != null && fast.next != null){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+
+    ListNode node = null;
+    while(slow!=null){
+        ListNode nextnode = slow.next;
+        slow.next=node;
+        node=slow;
+        slow=nextnode;
+
+    }
+
+     ListNode first = head;
+        ListNode second = node;
+
+        while (second != null) {
+            if (first.val != second.val) {
                 return false;
             }
-            st.pop();
-            temp=temp.next;
+
+            first = first.next;
+            second = second.next;
         }
+
         return true;
+
     }
+
 }
